a1c79c42696f7bd44e92092dafefbf3362b353b7
Guy <3strategy@gmail.com>
2021-09-15 15:16:16 +0300

    here is a much better non-blocking operation of the led.

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-#This code is for ESP32
+# This code is for ESP32
 from machine import Pin
 import time
 
 led = Pin(2, Pin.OUT)  # this is the internal ESP32 blue led.
 sw = Pin(0, Pin.IN)  # accessing the internal boot-select button.
 
-# another example of blinking the internal led.
+# non-blocking operation of the led.
+
+start_time = time.ticks_ms()
+interval = 500  # 500ms
+led_state = 0
 
 while True:
-    led(1)
-    time.sleep(0.6)  # this sleep code better resembles the more traditional MCU c code
-    led(0)
-    time.sleep(0.1)
-    print(sw.value(), 'gg2')
+    if time.ticks_ms() - start_time >= interval:
+        start_time = time.ticks_ms()
+        if led_state == 1:
+            led_state = 0
+        else:
+            led_state = 1
+
+    if sw.value() == 0:  # this loop is not blocked by sleep
+        # so even a quick press on the switch will output tons of prints
+        print('g4', time.ticks_ms())
+        led.value(1)
+    else:
+        led.value(led_state)
