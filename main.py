976c66314e203810cfa6ead3b8742ea054b4fb2f
Guy <3strategy@gmail.com>
2021-09-15 15:35:03 +0300

    low power bluetooth (BLE) example that communicates with phone terminal. Not Yet working.

--- main.py
+++ main.py
@@ -1,27 +1,103 @@
-# This code is for ESP32
 from machine import Pin
-import time
+from machine import Timer
+from time import sleep_ms
+import ubluetooth
 
-led = Pin(2, Pin.OUT)  # this is the internal ESP32 blue led.
-sw = Pin(0, Pin.IN)  # accessing the internal boot-select button.
+ble_msg = ""
 
-# non-blocking operation of the led.
 
-start_time = time.ticks_ms()
-interval = 500  # 500ms
-led_state = 0
+class ESP32_BLE():
+    def __init__(self, name):
+        # Create internal objects for the onboard LED
+        # blinking when no BLE device is connected
+        # stable ON when connected
+        self.led = Pin(2, Pin.OUT)
+        self.timer1 = Timer(0)
+
+        self.name = name
+        self.ble = ubluetooth.BLE()
+        self.ble.active(True)
+        self.disconnected()
+        self.ble.irq(self.ble_irq)
+        self.register()
+        self.advertiser()
+
+    def connected(self):
+        self.led.value(1)
+        self.timer1.deinit()
+
+    def disconnected(self):
+        self.timer1.init(period=100, mode=Timer.PERIODIC, callback=lambda t: self.led.value(not self.led.value()))
+
+    def ble_irq(self, event, data):
+        global ble_msg
+
+        if event == 1:  # _IRQ_CENTRAL_CONNECT:
+            # A central has connected to this peripheral
+            self.connected()
+
+        elif event == 2:  # _IRQ_CENTRAL_DISCONNECT:
+            # A central has disconnected from this peripheral.
+            self.advertiser()
+            self.disconnected()
+
+        elif event == 3:  # _IRQ_GATTS_WRITE:
+            # A client has written to this characteristic or descriptor.
+            buffer = self.ble.gatts_read(self.rx)
+            ble_msg = buffer.decode('UTF-8').strip()
+
+    def register(self):
+        # Nordic UART Service (NUS)
+        NUS_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'
+        RX_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
+        TX_UUID = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
+
+        BLE_NUS = ubluetooth.UUID(NUS_UUID)
+        BLE_RX = (ubluetooth.UUID(RX_UUID), ubluetooth.FLAG_WRITE)
+        BLE_TX = (ubluetooth.UUID(TX_UUID), ubluetooth.FLAG_NOTIFY)
+
+        BLE_UART = (BLE_NUS, (BLE_TX, BLE_RX,))
+        SERVICES = (BLE_UART,)
+        ((self.tx, self.rx,),) = self.ble.gatts_register_services(SERVICES)
+
+    def send(self, data):
+        self.ble.gatts_notify(0, self.tx, data + '\n')
+
+    def advertiser(self):
+        name = bytes(self.name, 'UTF-8')
+        adv_data = bytearray('\x02\x01\x02') + bytearray((len(name) + 1, 0x09)) + name
+        self.ble.gap_advertise(100, adv_data)
+        print(adv_data)
+        print("\r\n")
+        # adv_data
+        # raw: 0x02010209094553503332424C45
+        # b'\x02\x01\x02\t\tESP32BLE'
+        #
+        # 0x02 - General discoverable mode
+        # 0x01 - AD Type = 0x01
+        # 0x02 - value = 0x02
+
+        # https://jimmywongiot.com/2019/08/13/advertising-payload-format-on-ble/
+        # https://docs.silabs.com/bluetooth/latest/general/adv-and-scanning/bluetooth-adv-data-basics
+
+
+led = Pin(2, Pin.OUT)
+but = Pin(0, Pin.IN)
+ble = ESP32_BLE("ESP32BLE")
+
+
+def buttons_irq(pin):
+    led.value(not led.value())
+    ble.send('LED state will be toggled.')
+    print('LED state will be toggled.')
+
+
+but.irq(trigger=Pin.IRQ_FALLING, handler=buttons_irq)
 
 while True:
-    if time.ticks_ms() - start_time >= interval:
-        start_time = time.ticks_ms()
-        if led_state == 1:
-            led_state = 0
-        else:
-            led_state = 1
-
-    if sw.value() == 0:  # this loop is not blocked by sleep
-        # so even a quick press on the switch will output tons of prints
-        print('g4', time.ticks_ms())
-        led.value(1)
-    else:
-        led.value(led_state)
+    if ble_msg == 'read_LED':
+        print(ble_msg)
+        ble_msg = ""
+        print('LED is ON.' if led.value() else 'LED is OFF')
+        ble.send('LED is ON.' if led.value() else 'LED is OFF')
+    sleep_ms(100)
