e60864a9efcbf5d3cbfd01441aefdb7cbb5a75e1
Guy <3strategy@gmail.com>
2021-09-16 18:22:30 +0300

    Bluetooth HC-SC04 ultrasonic distance measure example. Added hscr04.py class file. Added keyboard exception handling.

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 # Bluetooth Low Energy with Servo.
-# Pin setup: Servo red to +5V (VIN),  black to Ground, signal (white or yellow) to D15
+# Pin setup: Servo IS DISCONNECTED HERE.    red to +5V (VIN),  black to Ground, signal (white or yellow) to D15
+# Pin setup hc-sr04.py red-vcc->3.3v, black-gnd->gnd, white-Trig->D13, brown-Echo-> D12
 from machine import Pin, PWM
 from time import sleep_ms
 from servosguy import Servo
 from esp32_ble import ESP32_BLE
+from hcsr04 import HCSR04
+from machine import Pin, I2C
 
 led = Pin(2, Pin.OUT)
 but = Pin(0, Pin.IN)
 ble = ESP32_BLE("ESP32BLE")
-servo1 = Servo(15, True)  # Servo(15, True,28,120,True)  # Servo(23, True) create a servo instance.
+# servo1 = Servo(15, True)  # Servo(15, True,28,120,True)  # Servo(23, True) create a servo instance.
+dist_sensor = HCSR04(trigger_pin=13, echo_pin=12, echo_timeout_us=1000000) #you can either 5v, gnd D13, D12, or 3.3v, gnd, 15, 2 (both options are adjacent pins).
 
 
 def buttons_irq(pin):
     toggle_led()
 
 
-def toggle_led():
+def toggle_led():  # this shows Android can control your device.
     led.value(not led.value())
     if led.value():
         s = ' On'
@@ -27,18 +31,23 @@ def toggle_led():
 
 
 but.irq(trigger=Pin.IRQ_FALLING, handler=buttons_irq)
-
-while True:
+try:
+  while True:
     bmsg = ble.msg
     ble.msg = ""  # this way we will not repeat acting on the message multiple times.
-    if bmsg == 'read_LED':
+    if bmsg == 'read_LED':  # phone is trying to read the Led state.
         print(bmsg)
         print('LED is ON.' if led.value() else 'LED is OFF')
         ble.send('LED is ON.' if led.value() else 'LED is OFF')
-    elif bmsg == 'servo_R':
-        servo1.right(7)
-    elif bmsg == 'servo_L':
-        servo1.left(7)
-    elif bmsg == 'tog_led':
+    # servo section. not in use in this example
+    # elif bmsg == 'servo_R':
+    #     servo1.right(7)
+    # elif bmsg == 'servo_L':
+    #     servo1.left(7)
+    elif bmsg == 'tog_led':  # phone is trying to toggle the led
         toggle_led()
+    elif bmsg == 'get_dist':  # phone requests distance
+        ble.send(f'distance is: {round(dist_sensor.distance_cm(),1)}')
     sleep_ms(100)  # Blocking code
+except KeyboardInterrupt:
+  pass
